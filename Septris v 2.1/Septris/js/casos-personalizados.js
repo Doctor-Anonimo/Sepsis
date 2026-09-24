a9edd3d1dc8c6210ecf4f62d8fe79347b9fc9496
Doctor-Anonimo <videoclasesjuanusc@gmail.com>
2026-09-24 14:58:27 -0500

    Delete Septris v 2.1/Septris directory

--- Septris v 2.1/Septris/js/casos-personalizados.js
+++
@@ -1,4 +0,0 @@
-// Añada aquí casos con el mismo formato de js/casos.js (ver LEEME.txt).
-// Cada caso necesita nombre único, historia, signos, estudios, tratamientos y desenlaces.
-// Se añaden al banco; cada partida elige hasta diez pacientes sin repetir.
-globalThis.CASOS_PERSONALIZADOS = [];
